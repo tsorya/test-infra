496e3789ba97a763c970bc6c81076b328c136403
Igal Tsoiref <itsoiref@redhat.com>
2020-04-27 13:24:05 +0300

    many new stuff

--- scripts/assisted_deployment.sh
+++ scripts/assisted_deployment.sh
@@ -25,10 +25,10 @@ function wait_for_cluster() {
 #TODO ADD ALL RELEVANT OS ENVS
 function run() {
   /usr/local/bin/skipper make $1 NUM_MASTERS=$NUM_MASTERS NUM_WORKERS=$NUM_WORKERS KUBECONFIG=$PWD/minikube_kubeconfig BASE_DOMAIN=$BASE_DOMAIN CLUSTER_NAME=$CLUSTER_NAME
-  if [ "$1" = "run_full_flow_with_install" ]; then
-    timeout 1h wait_for_cluster
-  fi
   if [ "${SET_DNS:-"n"}" == "y" ]; then
     set_dns
+  fi
+  if [ "$1" = "run_full_flow_with_install" ]; then
+    timeout 1h wait_for_cluster
   fi
 }
