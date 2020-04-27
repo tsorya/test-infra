b608665f80ee5da0356ed5bdeb6ca55458d17dc6
Igal Tsoiref <itsoiref@redhat.com>
2020-04-27 12:49:04 +0300

    many new stuff

--- scripts/assisted_deployment.sh
+++ scripts/assisted_deployment.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
 
-
-#TODO ADD ALL RELEVANT OS ENVS
-function run() {
-  /usr/local/bin/skipper make $1 NUM_MASTERS=$NUM_MASTERS NUM_WORKERS=$NUM_WORKERS KUBECONFIG=$PWD/minikube_kubeconfig BASE_DOMAIN=$BASE_DOMAIN CLUSTER_NAME=$CLUSTER_NAME
-  if [ "$1" = "run_full_flow_with_install" ]; then
-    wait_for_cluster
-  fi
-  if [ "${SET_DNS:-"n"}" == "y" ]; then
-    set_dns
-  fi
-}
-
-
 function destroy_all() {
     /usr/local/bin/skipper make destroy
 }
@@ -31,4 +18,15 @@ function wait_for_cluster() {
   done
   echo "Got 3 ready masters"
   echo -e "$(kubectl --kubeconfig=build/kubeconfig get nodes)"
+}
+
+#TODO ADD ALL RELEVANT OS ENVS
+function run() {
+  /usr/local/bin/skipper make $1 NUM_MASTERS=$NUM_MASTERS NUM_WORKERS=$NUM_WORKERS KUBECONFIG=$PWD/minikube_kubeconfig BASE_DOMAIN=$BASE_DOMAIN CLUSTER_NAME=$CLUSTER_NAME
+  if [ "$1" = "run_full_flow_with_install" ]; then
+    wait_for_cluster
+  fi
+  if [ "${SET_DNS:-"n"}" == "y" ]; then
+    set_dns
+  fi
 }
